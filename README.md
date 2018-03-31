3ab138a07011248075b3a627ca5bacd77c2ac55d
juansuerogit <juan.suero@gmail.com>
2018-03-31 19:37:03 -0400

    truffle 1

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # blockchaindevenv
 blockchain development environment to speed up brand new projects , nodejs, npm, typescript, webpack, react, truffle, solidity, web3.js, express, ganache, 
+
+
+
+install visual studio code
+sudo npm install truffle ganache-cli -g
+//replace solc that comes with truffle with latest solc
+
+npm install truffle --save-dev
+//replace solc that comes with truffle with latest solc
+
+
+make github project with gitignore, license and readme
+clone
+cd
+
+mkdir tmp
+cd tmp
+truffle unbox metacoin
+mv stuff from tmp to ../
+
+fix contracts to do  pragma solidity ^0.4.20;
+
+add truffle.js routes to blockchains
+
+npm install --save-dev truffle-contract
+npm install --save-dev babel-register
--- truffle.js
+++ truffle.js
@@ -1,4 +1,37 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+
+  networks: {
+    ganachegui: {
+      gas: 500,
+      host: "localhost",
+      port: 7545,
+      network_id: "*"
+    },
+    ganachecli: {
+      gas: 4712388,
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    },
+    mainnet: {
+      gas: 50000,
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    },
+    azure: {
+      gas: 500000,
+      host: "weiood-dns-reg1.eastus2.cloudapp.azure.com",
+      port: 8545,
+      network_id: "*"
+    }
+  }
+
+};
+
+mocha: {
+  useColors: true
 };
